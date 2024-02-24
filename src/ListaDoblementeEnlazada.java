032d3035572bfb2d267f46fb609507beaf85b0c0
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-02-23 20:27:42 -0600

    Creamos una clase en donde iran todas las opciones de insertar, recorrer, mostrar tamanio, etc

---
+++ src/ListaDoblementeEnlazada.java
@@ -0,0 +1,14 @@
+public class ListaDoblementeEnlazada {
+    Nodo inicio;
+    Nodo fin;
+    int tamanio;
+
+    public ListaDoblementeEnlazada(){
+        this.inicio = null;
+        this.fin = null;
+        this.tamanio = 0;
+    }
+
+
+
+}
