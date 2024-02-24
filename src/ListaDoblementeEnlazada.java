4e9634de933865bd7e2496e9b3ed2a1824aebcb4
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-02-23 20:40:41 -0600

    Creamos el método para insertar al inicio

--- src/ListaDoblementeEnlazada.java
+++ src/ListaDoblementeEnlazada.java
@@ -1,8 +1,13 @@
 public class ListaDoblementeEnlazada {
-    //variables de la clase nodo para
+    //Clase Lista Doblemente Enlazada
+    //Referencias a objetos de la clase Nodo
     Nodo inicio;
     Nodo fin;
+    //variable entera llamada tamanio que servirá para almacenar el tamaño de la lista enlazada
     int tamanio;
+
+    //Constructor de la clase en donde se inicializan los objetos inicio y fin como nulo
+    //Y el tamaño comienza en 0
 
     public ListaDoblementeEnlazada(){
         this.inicio = null;
@@ -10,18 +15,19 @@ public class ListaDoblementeEnlazada {
         this.tamanio = 0;
     }
 
+    //Método para insertar al Inicio que recibe como parámentro la variable dato
     public void insertarInicio(int dato){
-        Nodo nuevoNodo = new Nodo(dato);
+        Nodo nuevoNodo = new Nodo(dato); //Instanciamos un nuevo Nodo y abrimos una condicional en donde si la lista está vacia osea que inicio es igual a nulo,
+        //Si esto es así inicio y fin serían un nuevo nodo.
         if (inicio == null){
             inicio = nuevoNodo;
             fin = nuevoNodo;
-        } else {
+        } else { //Si la lista no está vacia, nuevoNodo.siguiente se establece como el nodo inicial
             nuevoNodo.siguiente = inicio;
-            inicio.anterior = nuevoNodo;
-            inicio = nuevoNodo;
+            inicio.anterior = nuevoNodo; //El inicio.anterior se establece como nuevoNodo
+            inicio = nuevoNodo; //Al igual que inicio se establece como nuevoNodo
         }
-        tamanio++;
-
+        tamanio++; // Y el tamaño de la lista aumenta.
     }
 
 
